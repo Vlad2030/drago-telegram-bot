314c81089d9042deba83be8d127db6964b8b4ce0
vladops <vlad123123v@gmail.com>
2024-01-05 01:39:25 +0300

    Update price.py

--- bot/handlers/messages/price.py
+++ bot/handlers/messages/price.py
@@ -1,6 +1,5 @@
 from aiogram import Router, types
 from aiogram.filters import Command
-from aiogram.utils.markdown import hcode
 
 from core.data.config import BotConfig
 from core.dex_screnner import DexScrenner
@@ -19,7 +18,6 @@ async def price_message(message: types.Message) -> types.Message:
     return await message.reply(
         text=f"🐲 ${token_info.baseToken.symbol}\n\n"
 
-             f"Price: {hcode(token_info.priceUsd, '$')} "
-             f"({hcode(token_info.priceNative, 'SOL')})\n\n",
+             f"Price: <code>{token_info.priceUsd}$ ({token_info.priceNative} SOL)</code>\n\n",
         disable_web_page_preview=True,
     )
