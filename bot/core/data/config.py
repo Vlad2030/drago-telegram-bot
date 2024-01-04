70aaa90001e3024d06f1907d6fea52152ff68f3c
vladops <vlad123123v@gmail.com>
2024-01-05 01:39:33 +0300

    Update config.py

--- bot/core/data/config.py
+++ bot/core/data/config.py
@@ -4,3 +4,5 @@ from utils.get_envs import Environs
 class BotConfig:
     BOT_TOKEN: str = Environs.get("TELEGRAM_BOT_TOKEN")
     DRAGO_CONTRACT_ADDRESS: str = Environs.get("DRAGO_CONTRACT_ADDRESS")
+    DRAGO_TELEGRAM_CHANNEL_ID_RU: str = Environs.get("DRAGO_TELEGRAM_CHANNEL_ID_RU")
+    DRAGO_TELEGRAM_CHANNEL_ID_EN: str = Environs.get("DRAGO_TELEGRAM_CHANNEL_ID_EN")
