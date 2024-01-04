39509b54d327c9bd8d11c10b8c3e1ff607f86976
vladops <vlad123123v@gmail.com>
2024-01-05 01:39:27 +0300

    Update info.py

--- bot/handlers/messages/info.py
+++ bot/handlers/messages/info.py
@@ -1,6 +1,5 @@
 from aiogram import Router, types
 from aiogram.filters import Command
-from aiogram.utils.markdown import hcode
 
 from core.data.config import BotConfig
 from core.dex_screnner import DexScrenner
@@ -20,26 +19,25 @@ async def info_message(message: types.Message) -> types.Message:
     return await message.reply(
         text=f"🐲 ${token_info.baseToken.symbol}\n\n"
 
-             f"Price: {hcode(token_info.priceUsd, '$')} "
-             f"({hcode(token_info.priceNative, 'SOL')})\n\n"
+             f"Price: <code>{token_info.priceUsd}$ ({token_info.priceNative} SOL)</code>\n\n"
 
-             f"Liquidity: {hcode(beatifier(token_info.liquidity.usd), '$')}\n"
-             f"FDV: {hcode(beatifier(token_info.fdv), '$')}\n"
-             f"Market Cap: {hcode(beatifier(token_info.fdv), '$')}\n\n"
+             f"Liquidity: <code>{beatifier(token_info.liquidity.usd)}$</code>\n"
+             f"FDV: <code>{beatifier(token_info.fdv)}$</code>\n"
+             f"Market Cap: <code>{beatifier(token_info.fdv)}$</code>\n\n"
 
              f"Price changes:\n"
-             f"5m: {hcode(token_info.priceChange.m5, '%')}\n"
-             f"1h: {hcode(token_info.priceChange.h1, '%')}\n"
-             f"6h: {hcode(token_info.priceChange.h6, '%')}\n"
-             f"24h: {hcode(token_info.priceChange.h24, '%')}\n\n"
+             f"5m: <code>{token_info.priceChange.m5}%</code>\n"
+             f"1h: <code>{token_info.priceChange.h1}%</code>\n"
+             f"6h: <code>{token_info.priceChange.h6}%</code>\n"
+             f"24h: <code>{token_info.priceChange.h24}%</code>\n\n"
 
              f"Volumes:\n"
-             f"5m: {hcode(beatifier(token_info.volume.m5), '$')}\n"
-             f"1h: {hcode(beatifier(token_info.volume.h1), '$')}\n"
-             f"6h: {hcode(beatifier(token_info.volume.h6), '$')}\n"
-             f"24h: {hcode(beatifier(token_info.volume.h24), '$')}\n\n"
+             f"5m: <code>{beatifier(token_info.volume.m5)}$</code>\n"
+             f"1h: <code>{beatifier(token_info.volume.h1)}$</code>\n"
+             f"6h: <code>{beatifier(token_info.volume.h6)}$</code>\n"
+             f"24h: <code>{beatifier(token_info.volume.h24)}$<code>\n\n"
 
-             f"CA: {hcode(token_info.baseToken.address)}\n\n"
+             f"CA: <code>{token_info.baseToken.address}</code>\n\n"
 
              f"{token_info.url}",
         disable_web_page_preview=True,
