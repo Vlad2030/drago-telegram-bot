4b02e5208d984abbb58f3bef7277e8e104ad3401
vladops <vlad123123v@gmail.com>
2024-01-05 01:39:29 +0300

    Update __init__.py

--- bot/handlers/messages/__init__.py
+++ bot/handlers/messages/__init__.py
@@ -1,9 +1,11 @@
 from .ca import router as ca_router
 from .price import router as price_router
 from .info import router as info_router
+from .new_post import router as new_post_router
 
 __all__ = [
     "ca_router",
     "price_router",
     "info_router",
+    "new_post_router",
 ]
