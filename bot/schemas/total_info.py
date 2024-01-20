be392aa8b18b3a3e1532bed02763bfb644368e9b
vladops <vlad123123v@gmail.com>
2024-01-21 02:00:07 +0300

    Add TotalInfo schema

---
+++ bot/schemas/total_info.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+
+class TotalInfo(BaseModel):
+    price: float
+    total_h24_volume: float
+    total_h24_volume_quote: float
+    total_h24_high: float
+    total_h24_low: float
+    total_ath: float
+    total_atl: float
