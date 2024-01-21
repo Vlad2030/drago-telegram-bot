4edb1de7a09bdf58468978ff121e87b1bbce9efc
vladops <vlad123123v@gmail.com>
2024-01-21 04:01:37 +0300

    Update beatifier method

--- bot/utils/number_beatifier.py
+++ bot/utils/number_beatifier.py
@@ -3,9 +3,9 @@ def beatifier(number: int | float) -> str:
     m_number = number / 1_000_000
     b_number = number / 1_000_000_000
     if k_number >= 1 and k_number < 1000:
-        return f"{k_number:.1f}k"
+        return f"{k_number:.2f}k"
     if m_number >= 1 and m_number < 1000:
-        return f"{m_number:.1f}m"
+        return f"{m_number:.2f}m"
     if b_number >= 1 and b_number < 1000:
-        return f"{b_number:.1f}b"
+        return f"{b_number:.2f}b"
     return "0"
