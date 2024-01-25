d294c06a34e16979adcc28448238a01d14532bc0
vladops <vlad123123v@gmail.com>
2024-01-25 06:24:39 +0300

    Add price_change attribut

--- bot/schemas/total_info.py
+++ bot/schemas/total_info.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 
 class TotalInfo(BaseModel):
     price: float
+    price_change: float
     total_h24_volume: float
     total_h24_volume_quote: float
     total_h24_high: float
